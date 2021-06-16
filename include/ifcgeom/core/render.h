7e5551eb8723b1fac2fd122e43b0b388e2a468bb
Bastian Nispel <71786901+bastiannispel@users.noreply.github.com>
2021-06-16 11:38:23 +0200

    Clang (#2)
    * fixed matching
    
    * clang-tidy clang-format

--- include/ifcgeom/core/render.h
+++ include/ifcgeom/core/render.h
@@ -6,6 +6,8 @@
 #include "utl/concat.h"
 #include "utl/parser/cstr.h"
 
+#include "IFC2X3/IfcRepresentationItem.h"
+
 #include "ifcgeom/geometric_repr_item.h"
 #include "ifcgeom/mapped_item.h"
 #include "ifcgeom/styled_item.h"
@@ -25,24 +27,27 @@ std::vector<Point_3> gather_product_vertices(context& ctx,
     return vec;
   }
 
-  for (auto const repr : p->Representation_.value()->Representations_) {
-    for (auto const item : repr->Items_) {
-      utl::concat(vec, match(item, solid_model_handler, polyline));
+  auto gather_vertices = [&](std::vector<IFC2X3::IfcRepresentationItem*> const& items) {
+    for (auto const item : items) {
+      utl::concat(
+        vec, match(item, topological_repr_item_handler,
+                   geometric_repr_item_handler, mapped_item, styled_item));
     }
+  };
+
+  for (auto const repr : p->Representation_.value()->Representations_) {
+    gather_vertices(repr->Items_);
   }
 
   auto shapes = repr_by_guid(ctx.element_part_map_, p->GlobalId_);
   for (auto const repr : shapes) {
-    for (auto const& repr : repr->Representations_) {
-      for (auto const& item : repr->Items_) {
-        utl::concat(
-            vec, match(item, topological_repr_item_handler,
-                       geometric_repr_item_handler, mapped_item, styled_item));
-      }
+    for (auto const repr : repr->Representations_) {
+      gather_vertices(repr->Items_);
     }
   }
   return vec;
-}std::vector<Point_3> gather_product_vertices(context& ctx,
+}
+std::vector<Point_3> gather_product_vertices(context& ctx,
                                              char const* c) {
   auto p = get_entity_by_guid<IFC2X3::IfcProduct>(ctx.model_, c);
   if(p.has_value()){
